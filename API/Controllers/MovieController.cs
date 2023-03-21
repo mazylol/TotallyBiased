3b6cebb7e39735900c5a2419e9be2e576e135c57
mazylol <landonp95@gmail.com>
2023-03-20 21:29:44 -0400

    Can add to db

--- API/Controllers/MovieController.cs
+++ API/Controllers/MovieController.cs
@@ -16,7 +16,6 @@ public class MovieController : Controller
     }
 
     [HttpGet]
-    // [ServiceFilter(typeof(ApiKeyAuthFilter))] future reference
     public async Task<ActionResult<Movie>> Get(string? shorthand)
     {
         if (shorthand == null)
@@ -25,27 +24,5 @@ public class MovieController : Controller
         }
 
         return await _mongoDbService.GetAsync(shorthand);
-    }
-
-    [HttpPost]
-    public async Task<string> Put()
-    {
-        var test = new Movie
-        {
-            Shorthand = "test",
-            Title = "Test",
-            Year = 2021,
-            Rating = "PG",
-            Runtime = "1h 30m",
-            Genre = "Comedy",
-            Director = "Test",
-            Actors = new[] { "Test", "Test" },
-            Thoughts = "Test",
-            Score = 5
-        };
-
-        await _mongoDbService.CreateAsync(test);
-
-        return "Put into the database";
     }
 }
