5e9dfdab4cf4b21ca3d90ef1e16a527ade24c3f7
Mazylol <landonp95@gmail.com>
2023-03-22 10:50:53 -0400

    looks bad but it works, and that is all that matters to me

--- Frontend/src/routes/admin/+page.svelte
+++ Frontend/src/routes/admin/+page.svelte
@@ -11,6 +11,7 @@
     let score: number;
 
     let auth: string;
+
     function handleClick() {
         let actors: string[] = unparsedActors?.split(',');
 
@@ -39,35 +40,43 @@
 
         title = '';
         shorthand = '';
-        year = 0;
+        year = undefined;
         rating = '';
         runtime = '';
         genre = '';
         director = '';
         unparsedActors = '';
         thoughts = '';
-        score = 0;
+        score = undefined;
     }
 </script>
 
 <div class="min-h-screen bg-gray-200">
-    <br />
+    <br/>
     <p class="font-bold text-center">Add Movies</p>
-    <br />
-    <div class="justify-center bg-white mx-4 rounded-md">
-        <input type="text" class="w-full h-12 px-4 my-1 rounded-md" placeholder="Title" bind:value={title} />
-        <input type="text" class="w-full h-12 px-4 my-1 rounded-md" placeholder="Shorthand" bind:value={shorthand} />
-        <input type="number" class="w-full h-12 px-4 my-1 rounded-md" placeholder="Year" bind:value={year} />
-        <input type="text" class="w-full h-12 px-4 my-1 rounded-md" placeholder="Rating" bind:value={rating} />
-        <input type="text" class="w-full h-12 px-4 my-1 rounded-md" placeholder="Runtime" bind:value={runtime} />
-        <input type="text" class="w-full h-12 px-4 my-1 rounded-md" placeholder="Genre" bind:value={genre} />
-        <input type="text" class="w-full h-12 px-4 my-1 rounded-md" placeholder="Director" bind:value={director} />
-        <input type="text" class="w-full h-12 px-4 my-1 rounded-md" placeholder="Actors" bind:value={unparsedActors} />
-        <input type="text" class="w-full h-12 px-4 my-1 rounded-md" placeholder="Thoughts" bind:value={thoughts} />
-        <input type="number" class="w-full h-12 px-4 my-1 rounded-md" placeholder="Score" bind:value={score} />
-    </div>
-    <br />
-    <input type="text" required class="w-full h-12 px-4 my-1 rounded-md mx-4 justify-center" placeholder="Authorization" bind:value={auth} />
-    <br />
-    <button class="w-full h-12 px-4 my-1 rounded-md mx-4 justify-center bg-blue-500 text-white" on:click={handleClick}>Add Movie</button>
+    <br/>
+
+    <form on:submit|preventDefault={handleClick}>
+        <div class="justify-center bg-white mx-4 rounded-md">
+            <input type="text" class="w-full h-12 px-4 my-1 rounded-md" placeholder="Title" bind:value={title}/>
+            <input type="text" class="w-full h-12 px-4 my-1 rounded-md" placeholder="Shorthand" bind:value={shorthand}/>
+            <input type="number" class="w-full h-12 px-4 my-1 rounded-md" placeholder="Year" bind:value={year}/>
+            <input type="text" class="w-full h-12 px-4 my-1 rounded-md" placeholder="Rating" bind:value={rating}/>
+            <input type="text" class="w-full h-12 px-4 my-1 rounded-md" placeholder="Runtime" bind:value={runtime}/>
+            <input type="text" class="w-full h-12 px-4 my-1 rounded-md" placeholder="Genre" bind:value={genre}/>
+            <input type="text" class="w-full h-12 px-4 my-1 rounded-md" placeholder="Director" bind:value={director}/>
+            <input type="text" class="w-full h-12 px-4 my-1 rounded-md" placeholder="Actors"
+                   bind:value={unparsedActors}/>
+            <input type="text" class="w-full h-12 px-4 my-1 rounded-md" placeholder="Thoughts" bind:value={thoughts}/>
+            <input type="number" class="w-full h-12 px-4 my-1 rounded-md" placeholder="Score" bind:value={score}/>
+        </div>
+        <br/>
+        <input type="text" required class="w-full h-12 mr-2 px-4 my-1 rounded-md mx-4 justify-center"
+               placeholder="Authorization" bind:value={auth}/>
+        <br/>
+        <input type="submit" class="justify-center h-8 w-32 bg-blue-500 hover:bg-blue-600 transition-all duration-100 text-white"
+               on:click={handleClick}/>
+    </form>
+
+
 </div>
